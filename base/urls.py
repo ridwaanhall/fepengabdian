1a7629145269eb00208db3cb8781df045a0dd2d1
ridwaanhall <erbyl14@gmail.com>
2024-08-06 19:31:51 +0700

    setup urls and views

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,19 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
+    # pamong
+    path('list-pamong/', views.list_pamong, name='list-pamong'),
+    path('detail-edit-pamong/', views.detail_edit_pamong, name='detail-edit-pamong'),
+    path('tambah-pamong/', views.tambah_pamong, name='tambah-pamong'),
+    
+    # user
+    path('list-user/', views.list_user, name='list-user'),
+    path('detail-edit-user/', views.detail_edit_user, name='detail-edit-user'),
+    path('tambah-user/', views.tambah_user, name='tambah-user'),
+    
+    # kegiatan
+    path('list-kegiatan/', views.list_kegiatan, name='list-kegiatan'),
+    path('detail-edit-kegiatan/', views.detail_edit_kegiatan, name='detail-edit-kegiatan'),
+    path('tambah-kegiatan/', views.tambah_kegiatan, name='tambah-kegiatan'),
 ]
--- base/views.py
+++ base/views.py
@@ -4,3 +4,35 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("About page")
+
+# pamong
+def list_pamong(request):
+    return render(request, 'list-pamong.html')
+
+def detail_edit_pamong(request):
+    return render(request, 'detail-edit-pamong.html')
+
+def tambah_pamong(request):
+    return render(request, 'tambah-pamong.html')
+
+
+# user
+def list_user(request):
+    return render(request, 'list-user.html')
+
+def detail_edit_user(request):
+    return render(request, 'detail-edit-user.html')
+
+def tambah_user(request):
+    return render(request, 'tambah-user.html')
+
+
+# kegiatan
+def list_kegiatan(request):
+    return render(request, 'list-kegiatan.html')
+
+def detail_edit_kegiatan(request):
+    return render(request, 'detail-edit-kegiatan.html')
+
+def tambah_kegiatan(request):
+    return render(request, 'tambah-kegiatan.html')
