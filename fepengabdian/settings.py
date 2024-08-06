3591c9b8639d73a1b08769ad99c38ab2a7a629bc
ridwaanhall <erbyl14@gmail.com>
2024-08-06 21:18:42 +0700

    register rest api and api

--- fepengabdian/settings.py
+++ fepengabdian/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-        
-    'api',
+    'rest_framework',
+    
+    'api.apps.ApiConfig',
     'base',
 ]
 
